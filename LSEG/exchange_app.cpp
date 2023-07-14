e323ad460c4b90cfdc5f9114124ba2f45f6f9f49
Vijithan Kurrshanth <109568325+VijthanKurrshanth@users.noreply.github.com>
2023-07-14 23:48:11 +0530

    modified:   LSEG/exchange_app.cpp 	modified:   LSEG/exchange_app.exe

--- LSEG/exchange_app.cpp
+++ LSEG/exchange_app.cpp
@@ -6,22 +6,28 @@
 
 struct in_ord{
 
-std::string c_ord_id ;
-std::string inst ;
+std::string c_ord_id,inst,ord_id;
 int side, qty;
 double price;
 
 };
 
+std::string intToHex(int number) {
+    std::stringstream stream;
+    stream << std::hex << number;  // Convert int to hexadecimal string
+    return stream.str();  // Get the string representation
+}
+
 /// creates in_ord struct using the row in the input order
 /// returns the in_ord struct pointer 
-in_ord* record(std::vector<std::string> row){
+in_ord* record(std::vector<std::string> row, int order_no){
     in_ord* order = new in_ord;
     order->c_ord_id = row[0];
     order->inst = row[1];
     order->side = std::stoi(row[2]);
     order->price = std::stod(row[3]);
     order->qty = std::stoi(row[4]);
+    order->ord_id = intToHex(order_no);
 
     return order;
 }
@@ -40,6 +46,7 @@ int main(){
     fin.open("orders1.csv");
 
     int line_no = 1;
+    int order_no = 1;
     std::string line, word;
     std::vector<std::string> row;
     while (std::getline(fin, line)) {
@@ -63,6 +70,69 @@ int main(){
             row.push_back(word);
         }
 
+        
+        in_ord* order = record(row, order_no);
+        order_no += 1;
+
+        if (order->inst == "Rose"){
+            switch (order->side){
+                case 1:
+                blue_list_rose.push_back(order);
+                break;
+                case 2:
+                pink_list_rose.push_back(order);
+                break;
+            }
+
+        }
+        else if (order->inst == "Lavender"){
+            switch (order->side){
+                case 1:
+                blue_list_lavender.push_back(order);
+                break;
+                case 2:
+                pink_list_lavender.push_back(order);
+                break;
+            }
+
+        }
+        else if (order->inst == "Lotus"){
+            switch (order->side){
+                case 1:
+                blue_list_lotus.push_back(order);
+                break;
+                case 2:
+                pink_list_lotus.push_back(order);
+                break;
+            }
+
+        }
+        else if (order->inst == "Tulip"){
+            switch (order->side){
+                case 1:
+                blue_list_tulip.push_back(order);
+                break;
+                case 2:
+                pink_list_tulip.push_back(order);
+                break;
+            }
+
+        }
+        else if (order->inst == "Orchid"){
+            switch (order->side){
+                case 1:
+                blue_list_orchid.push_back(order);
+                break;
+                case 2:
+                pink_list_orchid.push_back(order);
+                break;
+            }
+
+        }
+        else{
+
+        }
+        
         // Print line (read from file) in Console
         std::cout << line << std::endl;
 
